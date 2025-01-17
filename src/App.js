c6cdfe62bd7e7c4b2060e65fda8f3d854c961165
dongsubnambuk <ehdtjq1278@naver.com>
2025-01-17 17:16:49 +0900

    ✅ main화면 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Todo from './pages/todo';
-
+import MainPage from './pages/MainPage';
 function App() {
   return (
-    <div className="App">
-     <Todo />
-    </div>
+    <Router>
+      <div className="mobile-container">
+        <Routes>
+          <Route path="/todo" element={<Todo />} />
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import '../CSS/MainPage.css';
+import logo from'../images/todologo.png';
+import { useNavigate } from 'react-router-dom';
+const MainPage = () => {
+    const navigate = useNavigate();
+
+    const handleLoginClick = () => {
+        navigate('/login');
+    };
+
+    const handleSignupClick = () => {
+        navigate('/signup');
+    };
+
+
+
+
+return (
+  <div className='main-page-container'>
+
+    <div className='main-page-title-container'>
+        <img className='main-page-title-logo' src={logo} alt='logo'/>
+        <p className='main-page-title-content'>기깔나는 todo와 함께 <br/> 일상을 기록해보세요.</p>
+    </div>
+
+    <div className='main-page-btn-container'>
+        <button className='main-page-signup-btn' onClick={handleSignupClick}>
+            계정없냐? 회원가입 ㄱ
+        </button>
+        <button className='main-page-login-btn' onClick={handleLoginClick}>
+             계정이 있냐? 로그인 ㄱ
+        </button>
+    </div>
+  </div>
+
+    );
+}
+
+export default MainPage;
